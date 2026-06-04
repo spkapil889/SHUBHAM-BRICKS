d9e2ea5afd03a7427fbc6bdd5eb9b648a6f8527b
Shashank Pathak <61456653+spkapil889@users.noreply.github.com>
2026-06-04 11:15:48 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -77,7 +77,10 @@ export default function Footer() {
               <li className="flex gap-4">
                 <MapPin className="text-brand-red shrink-0" size={20} />
                 <p className="text-gray-400 text-sm">
-Plot No. 43, Vijay Complex, Shop No. 2,<br> near Bhagat Singh Circle, Scheme No. 2, <br>Alwar, Rajasthan – 301001             
+              Plot No. 43, Vijay Complex, Shop No. 2,
+                near Bhagat Singh Circle, Scheme No. 2,
+                  Alwar, Rajasthan – 301001
+              </p>
               </li>
               <li className="flex gap-4">
                 <Phone className="text-brand-red shrink-0" size={20} />
